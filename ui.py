9bb649f6bb4decec296d4fffd64989b769084e8a
Shubham Rathore <87930554+shoe-bham-cries@users.noreply.github.com>
2021-10-27 21:42:57 +0530

    Add files via upload

--- ui.py
+++ ui.py
@@ -1,30 +1,73 @@
-import tkinter as tki
+from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
-class QuizUI:
-    def __init__(self):
-        self.window = tki.Tk()
-        self.window.title("QuTia")
-        self.window.minsize(width=350, height=500)
-        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+class QuizInterface:
 
-        self.scorelabel = tki.Label(text="Score", fg="white", bg=THEME_COLOR)
-        self.scorelabel.grid(column=1, row=0)
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
 
-        self.canvas = tki.Canvas(width=300, height=250)
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
         self.question_text = self.canvas.create_text(
             150,
             125,
-            text="placeholder",
+            width=280,
+            text="Some Question Text",
             fill=THEME_COLOR,
-            font=('Arial', 20, 'italic'))
-        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
-        true_image = tki.PhotoImage("images/true.png")
-        self.true = tki.Button(image=true_image, highlightthickness=0, height=100, width=100)
-        self.true.grid(column=0, row=2)
-        false_image = tki.PhotoImage('images/false.png')
-        self.false = tki.Button(image=false_image)
-        self.false.grid(column=1, row=2)
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_pressed)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_pressed)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+
+
+
+
+
+
+
+
